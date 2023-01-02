afeb94549c53209098f968d9d0de6850efb3a087
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-01-02 21:26:40 +0530

    optimal

--- 10tutorial/10uniquenooccu.cpp
+++ 10tutorial/10uniquenooccu.cpp
@@ -1,1 +1,52 @@
-//
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool uniqueOccurrences(vector<int> &arr)
+{
+    vector<int> ans;
+    int size = arr.size();
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < size;)
+    {
+        int count = 1;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+            ans.push_back(count);
+            i = i + count;
+        }
+    }
+
+    size = ans.size();
+    sort(ans.begin(), ans.end());
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (ans[i] == ans[i + 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+
+    vector<int> arr = {1, 2, 2, 1, 1, 3};
+
+    cout << uniqueOccurrences(arr);
+
+    
+}
