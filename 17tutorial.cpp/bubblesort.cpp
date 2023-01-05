72d1f05099a0d72c20182e25630f1476d09810ec
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-01-05 12:16:53 +0530

    17

---
+++ 17tutorial.cpp/bubblesort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+void bubbleSort(vector<int>& arr, int n)
+{   
+   for(int i = 1; i<n; i++)
+   {
+       for(int j = 0; j< n-i; j++)
+       {
+           if(arr[j]>arr[j+1])
+               swap(arr[j], arr[j+1]);
+       }
+   }
+}
+
+ 
+int main(){
+ 
+  
+  
+return 0;
+}
