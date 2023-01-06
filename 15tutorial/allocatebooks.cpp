247072046c9ec48f8fb518df7ffb66dd806c4a9e
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-01-06 15:59:36 +0530

    painters

--- 15tutorial/allocatebooks.cpp
+++ 15tutorial/allocatebooks.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
-#include<vector>
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 bool isPossible(vector<int> arr, int n, int m, int mid)
 {
+
     int studentCount = 1;
-    int pageSum =0;
-    for(int i = 0; i<n; i++)
+    int pageSum = 0;
+    for (int i = 0; i < n; i++)
     {
-        if(pageSum + arr[i] <= mid)
+        if (pageSum + arr[i] <= mid)
         {
             pageSum += arr[i];
         }
         else
         {
             studentCount++;
-            if(studentCount > m || arr[i] > mid)
+            if (studentCount > m || arr[i] > mid)
             {
                 return false;
             }
@@ -27,43 +28,44 @@ bool isPossible(vector<int> arr, int n, int m, int mid)
     return true;
 }
 
-int allocateBooks(vector<int> arr, int n, int m) {
-   int s = 0;
+int allocateBooks(vector<int> arr, int n, int m)
+{
+    int s = 0;
     int sum = 0;
-    for(int i=0; i<n; i++){
-        sum+=arr[i];
-    }
-   int e = sum;
-    int ans = -1;
-    int mid = s + (e-s)/2;
-    
-    while(s<=e)
+    for (int i = 0; i < n; i++)
     {
-        if(isPossible(arr, n, m, mid))
+        sum += arr[i];
+    }
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e - s) / 2;
+
+    while (s <= e)
+    {
+        if (isPossible(arr, n, m, mid))
         {
-            ans = mid; 
-            e = mid-1;
+            ans = mid;
+            e = mid - 1;
         }
         else
         {
             s = mid + 1;
         }
-        mid = s + (e-s)/2;
+        mid = s + (e - s) / 2;
     }
-return ans;
+    return ans;
 }
- 
-int main(){
- 
-  vector<int> v = { 10, 20, 10, 10, 20, 10};
-  /* v.push_back(10);  // 10, 190 = 190
-  v.push_back(60);  // 70, 130 = 130
-  v.push_back(40);  // 110, 90 = 110
-  v.push_back(60);  // 170, 30 = 170
-  v.push_back(30);  
 
- */
- cout<<allocateBooks(v, v.size(), 2);
-  
+int main()
+{
 
+    vector<int> v = {10, 20, 10, 10, 20, 10};
+    /* v.push_back(10);  // 10, 190 = 190
+    v.push_back(60);  // 70, 130 = 130
+    v.push_back(40);  // 110, 90 = 110
+    v.push_back(60);  // 170, 30 = 170
+    v.push_back(30);
+
+   */
+    cout << allocateBooks(v, v.size(), 2);
 }
