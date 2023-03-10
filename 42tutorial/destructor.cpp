1b466941ec76f4e36b5cdd1eab9db510a6bd1319
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-03-10 14:59:47 +0530

    !

--- 42tutorial/destructor.cpp
+++ 42tutorial/destructor.cpp
@@ -1,10 +1,93 @@
-#include<iostream>
+#include <iostream>
+#include<cstring>
 using namespace std;
- 
- 
-int main(){
- 
-  
-  
-return 0;
+
+class Employee
+{
+private:
+    int health;
+
+public:
+    char *name;
+    char level;
+
+    Employee()
+    {
+        cout << "Constructor called" << endl;
+        name = new char[100];
+    }
+
+    // parameterized constructor
+    Employee(int health)
+    {
+        this->health = health;
+    }
+
+    Employee(int health, char level)
+    {
+        this->health = health;
+        this->level = level;
+    }
+
+    // copy constructor
+    Employee(Employee &temp)
+    {
+        char *ch = new char[strlen(temp.name) + 1];
+        strcpy(ch, temp.name);
+        this->name = ch;
+
+        this->health = temp.health;
+        this->level = temp.level;
+    }
+
+    void print()
+    {
+        cout << "Name : " << this->name << endl;
+        cout << "Health : " << this->health << endl;
+        cout << "Level : " << this->level << endl;
+        cout << endl;
+    }
+
+    int getHealth()
+    {
+        return health;
+    }
+
+    int getLevel()
+    {
+        return level;
+    }
+
+    // Setters
+    void setHealth(int h)
+    {
+        health = h;
+    }
+
+    void setLevel(char ch)
+    {
+        level = ch;
+    }
+
+    void setName(char name[])
+    {
+        strcpy(this->name, name);
+    }
+    
+    //destructor
+    ~Employee(){
+            cout<<"Destructor called"<<endl;
+    }
+   
+};
+
+int main()
+{
+     
+    //static
+    Employee emp1;
+
+    // dynamic
+    Employee *emp2 = new Employee();
+    return 0;
 }
