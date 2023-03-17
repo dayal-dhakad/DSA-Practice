024c3ea52143348b9b84ee9dd6225e782856e2ec
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-03-17 15:46:58 +0530

    queue

---
+++ 60tutorial/queue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+ 
+ 
+int main(){
+ queue<int> q;
+ q.push(11);
+ q.push(15);
+ q.push(17);
+
+cout<<"Size of queue is "<<q.size()<<endl;
+  
+return 0;
+}
