9dc3e7095901543e400087e9741bd1b737ccb8b3
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-03-17 15:50:13 +0530

    !

--- 60tutorial/queue.cpp
+++ 60tutorial/queue.cpp
@@ -6,10 +6,24 @@ using namespace std;
 int main(){
  queue<int> q;
  q.push(11);
+ cout<<"Front of queue is : "<<q.front()<<endl;
  q.push(15);
+  cout<<"Front of queue is : "<<q.front()<<endl;
+
  q.push(17);
+ cout<<"Front of queue is : "<<q.front()<<endl;
 
 cout<<"Size of queue is "<<q.size()<<endl;
-  
+q.pop();
+  cout<<"Size of queue is "<<q.size()<<endl;
+if(q.empty()){
+    cout<<"Queue is empty"<<endl;
+}
+else{
+    cout<<"Queue is not empty"<<endl;
+}
+
+
+
 return 0;
 }
