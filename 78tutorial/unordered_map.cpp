436611a7b3ad92ecdc096d735066eb21acbcf93d
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-07-03 17:02:06 +0530

    unordered_map

--- 78tutorial/unordered_map.cpp
+++ 78tutorial/unordered_map.cpp
@@ -11,8 +11,14 @@ int main(){
  unordered_map<string, int> m;
 
 //insertion
+//1 method
+pair<string, int> p1 = make_pair("Dayal", 3);
+
+//2 method
+pair<string, int> p2("Dhakad", 2);
  
-  
+ //3 method 
+ m["DD"] = 5;
   
 return 0;
 }
