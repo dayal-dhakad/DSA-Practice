cd5e4df05edcdcd4b818aec8f881daf03fcc6348
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-07-06 16:36:49 +0530

    BS

--- 33tutorial/binarySearchRecursion.cpp
+++ 33tutorial/binarySearchRecursion.cpp
@@ -1,5 +1,28 @@
 #include<iostream>
 using namespace std;
+bool binarySearch(int arr[], int s, int e, int key){
+ 
+  if (s>e)
+  {
+     return false;
+  }
+
+ int mid = s + (e-s)/2;
+ if (arr[mid]==key)
+ {
+  return true;
+ }
+ 
+ if (arr[mid]< key)
+ {
+  return binarySearch(arr, mid+1, e, key);
+ }
+ else{
+  return binarySearch(arr, s, mid-1, key);
+ }
+ 
+  
+}
  
   
  
