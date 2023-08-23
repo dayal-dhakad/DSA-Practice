7747407d556c234c69264445f5621f29515eeadf
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-08-23 14:10:13 +0530

    Update

--- LeetCode/reorganizesting.cpp
+++ LeetCode/reorganizesting.cpp
@@ -1,41 +1,34 @@
-#include<iostream>
-#include<string>
-using namespace std;
- 
-  string reorganizeString(string s) {
-        int i=0;
-        int j=1;
-m:
-        while(j<s.size()){
-            
-            if(s[i]==s[j]){
-                i++;
-                j++;
-                
-                if(s[i]!=s[j]){
-                    swap(s[i], s[j]);
-                    
-                }
-            }
-            else{
-                i++;
-                j++;
-                goto m;
-            }
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> freq_map;
+        for (char c : s) {
+            freq_map[c]++;
         }
-        for(int i= 0; i<s.size(); i++){
-            if(s[i]==s[i+1]){
-                return "x";
-            }
+
+        priority_queue<pair<int, char>> max_heap;
+        for (auto &[ch, freq] : freq_map) {
+            max_heap.push({freq, ch});
         }
-        return s;
+
+        string res;
+        while (max_heap.size() >= 2) {
+            auto [freq1, char1] = max_heap.top(); max_heap.pop();
+            auto [freq2, char2] = max_heap.top(); max_heap.pop();
+
+            res += char1;
+            res += char2;
+
+            if (--freq1 > 0) max_heap.push({freq1, char1});
+            if (--freq2 > 0) max_heap.push({freq2, char2});
+        }
+
+        if (!max_heap.empty()) {
+            auto [freq, ch] = max_heap.top();
+            if (freq > 1) return "";
+            res += ch;
+        }
+
+        return res;
     }
- 
-int main(){
- 
- string s = "aaabb";
- cout<<reorganizeString(s)<<endl;
-  
-  
-return 0;
-}
+};
