f5d7baf9979404331324dd4c66f4557a297bdf20
Deendayal Dhakad <dayal.dhakad.125@gmail.com>
2023-09-06 13:35:14 +0530

    Power

--- Recursion/power.cpp
+++ Recursion/power.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 using namespace std;
- 
+
+int power(int num, int i){
+    if(i==1){
+        return num;
+    }
+
+    return num * power(num, i-1);
+
+}
  
 int main(){
  
-  
+  int num = 5;
+  int i = 3;
+
+  cout<<power(num, i)<<endl;
   
 return 0;
 }
